f81be95d93defba997a8fc826c6cd59a2d10f647
anagha8665 <anaghakj2001@gmail.com>
2022-10-16 10:54:17 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AgileComponent } from './agile/agile.component';
+import { IotComponent } from './iot/iot.component';
+import { InnovationComponent } from './innovation/innovation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgileComponent,
+    IotComponent,
+    InnovationComponent
   ],
   imports: [
     BrowserModule
